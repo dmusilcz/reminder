dd920105ee018f6dd2ef4f1322f4ca959a5bf482
David Musil <dmusilcz@gmail.com>
2020-12-14 21:50:10 +0100

    Updated signup form using feedback Optimized language choices

--- reminder/settings.py
+++ reminder/settings.py
@@ -150,7 +150,7 @@ LOCALE_PATHS = (
 )
 
 LANGUAGES = [
-    ('cs', _('Czech')),
+    ('cs', 'Česky'),
     ('en', _('English')),
 ]
 
@@ -159,7 +159,7 @@ EXTRA_LANG_INFO = {
         'bidi': False, # right-to-left
         'code': 'cs',
         'name': 'Czech',
-        'name_local': u'Čeština', #unicode codepoints here
+        'name_local': u'Česky', #unicode codepoints here
     },
 }
 
